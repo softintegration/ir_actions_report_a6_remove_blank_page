30eed04547d0854d438ee39ade88c674730e4d86
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-02-08 22:33:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Remove A6 format reports blank pages
+------------------------------------
+This module remove the blank pages printed in the reports of the format A6. 
+
+
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Remove A6 format reports blank pages',
+    'author': 'Soft-integration',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Hidden',
+    'demo': [],
+    'depends': ['base'],
+    'data': [
+    ],
+    'license': 'LGPL-3',
+}
